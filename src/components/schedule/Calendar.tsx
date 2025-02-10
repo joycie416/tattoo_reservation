cdf86ba305cf5d935b59b903a96286b549b4452e
joycie416 <joycie416@gmail.com>
2025-02-10 22:41:05 +0900

    style: 달력 스타일 수정

--- src/components/schedule/Calendar.tsx
+++ src/components/schedule/Calendar.tsx
@@ -22,19 +22,21 @@ const Calendar = ({
   const calendar = getOneMonth(currentMonth);
 
   return (
-    <div>
-      <div>
-        <button onClick={prevMonth}>prev</button>
+    <div className="mx-5 flex flex-col items-center">
+      <div className="flex gap-3">
+        <button onClick={prevMonth}>{"<"}</button>
         <p>
           {currentMonth[0]}.{("0" + currentMonth[1]).slice(-2)}
         </p>
-        <button onClick={nextMonth}>next</button>
+        <button onClick={nextMonth}>{">"}</button>
       </div>
-      {calendar.map((week, i) => (
-        <div key={`week_${i}`} className="grid grid-cols-7">
-          {week.map(dateFn)}
-        </div>
-      ))}
+      <div className="w-full">
+        {calendar.map((week, i) => (
+          <div key={`week_${i}`} className="grid grid-cols-7">
+            {week.map(dateFn)}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
