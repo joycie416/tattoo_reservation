b43b1008ec464facd057c8932433065cd265609f
joycie416 <joycie416@gmail.com>
2025-03-29 18:41:51 +0900

    fix: 빌드 에러 해결

--- src/components/schedule/AddScheduleForm.tsx
+++ src/components/schedule/AddScheduleForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import TimeButton from "../shared/TimeButton";
 import { cn } from "@/lib/utils";
 import AddScheduleButton from "./AddScheduleButton";
