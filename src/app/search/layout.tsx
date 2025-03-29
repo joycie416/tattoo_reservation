56dfaf512b0175e46a05b295d206767e701efe41
joycie416 <joycie416@gmail.com>
2025-03-29 18:28:40 +0900

    update: 예약 내역 카드 열림닫힘 기능 추가, 열림닫힘 에 따라 텍스트 스타일 다르게

--- src/app/search/layout.tsx
+++ src/app/search/layout.tsx
@@ -1,5 +1,14 @@
 import { Provider } from "jotai";
+import { Metadata } from "next";
 import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "enan.tt 예약 확인",
+  description: "타투 예약 내역을 확인합니다.",
+  openGraph: {
+    description: "타투 예약 내역을 확인합니다.",
+  },
+};
 
 const ReservationSearchLayout = ({ children }: { children: ReactNode }) => {
   return <Provider>{children}</Provider>;
--- src/components/shared/PageHeader.tsx
+++ src/components/shared/PageHeader.tsx
@@ -6,7 +6,7 @@ import LeftArrow from "/public/icons/left-arrow.svg";
 type PageHeaderProps = { title: string; backTo: string };
 const PageHeader = ({ title, backTo }: PageHeaderProps) => {
   return (
-    <header className="w-full h-[59px] sticky top-0 flex justify-center items-center bg-white border-b-[3px] border-background text-font1 text-title-lg text-center relative">
+    <header className="w-full h-[59px] sticky top-0 flex justify-center items-center bg-white border-b-[3px] border-background text-font1 text-title-lg text-center relative z-10">
       <Link href={backTo} className="absolute left-5">
         <Image
           src={LeftArrow}
