5177e2a0121adc5c7359e37e829f4a060a4db647
joycie416 <joycie416@gmail.com>
2025-04-02 18:47:42 +0900

    style: 로그인 페이지 style 및 disable state 추가

--- src/components/admin/AdminLoginForm.tsx
+++ src/components/admin/AdminLoginForm.tsx
@@ -2,9 +2,12 @@
 
 import { login } from "@/api/auth";
 import { LoginType } from "@/types/auth";
+import { useState } from "react";
 
 const AdminLoginForm = () => {
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const [disable, setDisable] = useState(true);
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const data = Object.fromEntries(formData.entries());
@@ -12,34 +15,43 @@ const AdminLoginForm = () => {
     console.error(error);
 
     if (error) {
-      alert(`${error.name}: ${error.code}`);
+      alert(`로그인 실패: ${error.name}`);
     }
+  };
+
+  const onChange = (e: React.FormEvent<HTMLFormElement>) => {
+    const formData = new FormData(e.currentTarget);
+    setDisable(!formData.get("id") || !formData.get("password"));
   };
 
   return (
     <div className="w-full self-center">
       <form
-        onSubmit={handleSubmit}
-        className="w-full p-5 flex flex-col gap-4 bg-white border border-guide rounded-md"
+        onSubmit={onSubmit}
+        onChange={onChange}
+        className="w-full flex flex-col gap-6"
       >
-        <h2 className="mx-auto">관리자 로그인</h2>
-        <div className="flex flex-col">
-          <label>이메일</label>
-          <input
-            type="email"
-            name="email"
-            className="px-3 py-2 border border-background rounded-md"
-          />
+        <h2 className="mx-auto text-title-lg">로그인해주세요</h2>
+        <div className="space-y-2">
+          <div className="flex flex-col gap-2">
+            <label className="text-subtitle-md">사용자 아이디</label>
+            <input type="id" name="id" placeholder="아이디를 입력해주세요" />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label className="text-subtitle-md">비밀번호</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="영문, 숫자, 특수문자를 포함한 비밀번호를 입력해주세요"
+            />
+          </div>
         </div>
-        <div className="flex flex-col">
-          <label>비밀번호</label>
-          <input
-            type="password"
-            name="password"
-            className="px-3 py-2 border border-background rounded-md"
-          />
-        </div>
-        <button className="h-[42px] bg-button rounded-md">로그인</button>
+        <button
+          disabled={disable}
+          className="h-11 bg-gray-70 rounded-md text-button-md text-gray-10 disabled:bg-gray-30"
+        >
+          로그인
+        </button>
       </form>
     </div>
   );
